70e4051e2ffa9483bf2833e2d504bb06843d4927
NasserSami <salamat247@gmail.com>
2025-03-13 23:52:29 -0400

    Add project files.

---
+++ WordleGameClient/WordServer.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace WordleGameClient
+{
+    public class WordServer
+    {
+        private const string FileName = "wordle.json";
+        private static readonly WordServer[] _words;
+
+        static WordServer()
+        {
+            try
+            {
+               var json = File.ReadAllText(FileName);
+               _words = JsonConvert.DeserializeObject<WordServer[]>(json)!;
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Error reading file!");
+            }
+
+
+        }
+
+        public string GetWord()
+        {
+
+
+
+
+
+            return "Hello, World!";
+        }
+
+    }
+}
